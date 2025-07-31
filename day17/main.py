4082c9e9dd61933afd3227b8277bd8ed8b8ba0c3
Ritsu- <biasonneilaldous@gmail.com>
2025-07-31 12:17:31 +0800

    day17 done!

--- day17/main.py
+++ day17/main.py
@@ -1,5 +1,5 @@
 from data import question_data
-
+from quiz_brain import QuizBrain
 from question_model import Question
 
 question_bank = []
@@ -7,3 +7,8 @@ question_bank = []
 for question in question_data:
     new_question = Question(question["text"], question["answer"])
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+quiz.next_question()
+
--- day17/quiz_brain.py
+++ day17/quiz_brain.py
@@ -1,2 +1,32 @@
+from tkinter import BooleanVar
+
+
 class QuizBrain:
-    def __init__(self,n 
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_questions(self) -> bool:
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        while self.still_has_questions():
+            user_answer = input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False): ")
+            self.check_answer(user_answer)
+        else:
+            response = input(f"Quiz Over! Final Score: {self.score}\nPlay Again? (Y/N): ")
+
+            if response == "Y":
+                self.question_number = 0
+                self.score = 0
+                self.next_question()
+
+    def check_answer(self, answer):
+        if answer == self.question_list[self.question_number].answer:
+            self.score += 1
+            print("Correct!")
+        else:
+            print("Incorrect!")
+        self.question_number += 1
+        print(f"Score: {self.score}/{self.question_number}\n")
